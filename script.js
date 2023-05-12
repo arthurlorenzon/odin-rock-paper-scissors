f651fdc20f272edbea9665dd11b2eb81909e41f2
Arthur Lorenzon <121390986+arthurlorenzon@users.noreply.github.com>
2023-05-12 10:44:41 -0300

    Add UI and remove alert() functions

--- script.js
+++ script.js
@@ -1,60 +1,164 @@
+//GAME
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = '';
+
 function computerPlay() {
     let num1 = Math.random();
     if (num1 < 0.333) {
       return "ROCK";
-    } else if (0.333 < num1 && num1 < 0.666) {
+    } else if (0.333 <= num1 && num1 < 0.666) {
       return "PAPER";
     } else if (num1 > 0.666) {
       return "SCISSORS";
     }
   }
   
-  function playerPlay() {
-    const ask = prompt("Rock, paper, or scissors?").toUpperCase();
-    return ask;
+function playRound(playerPlay, computerPlay) {
+  if (playerPlay == computerPlay) {
+    roundWinner = 'tie';
+  } else if (
+    (computerPlay == "SCISSORS" && playerPlay == "ROCK") ||
+    (computerPlay == "PAPER" && playerPlay == "SCISSORS") ||
+    (computerPlay == "ROCK" && playerPlay == "PAPER")
+  ) {
+    playerScore++;
+    roundWinner = 'player';
+  } else if (
+    (computerPlay == "SCISSORS" && playerPlay == "PAPER") ||
+    (computerPlay == "PAPER" && playerPlay == "ROCK") ||
+    (computerPlay == "ROCK" && playerPlay == "SCISSORS")
+  ) {
+    computerScore++;
+    roundWinner = 'computer';
   }
-  
-  function playRound(playerPlay, computerPlay) {
-    if (playerPlay == computerPlay) {
-      return "tie";
-    } else if (
-      (computerPlay == "SCISSORS" && playerPlay == "ROCK") ||
-      (computerPlay == "PAPER" && playerPlay == "SCISSORS") ||
-      (computerPlay == "ROCK" && playerPlay == "PAPER")
-    ) {
-      return "player";
-    } else if (
-      (computerPlay == "SCISSORS" && playerPlay == "PAPER") ||
-      (computerPlay == "PAPER" && playerPlay == "ROCK") ||
-      (computerPlay == "ROCK" && playerPlay == "SCISSORS")
-    ) {
-      return "computer";
-    }
+  updateScoreMessage(roundWinner, playerPlay, computerPlay)
+}
+
+function gameOver() {
+  return playerScore === 5 || computerScore === 5
+}
+
+//UI
+const scoreInfo = document.getElementById('scoreInfo')
+const scoreMessage = document.getElementById('scoreMessage')
+const playerScorePara = document.getElementById('playerScore')
+const computerScorePara = document.getElementById('computerScore')
+const playerChose = document.getElementById('playerChose')
+const cpuChose = document.getElementById('cpuChose')
+const rockButton = document.getElementById('rockButton')
+const paperButton = document.getElementById('paperButton')
+const scissorsButton = document.getElementById('scissorsButton')
+const endgameModal = document.getElementById('endgameModal')
+const endgameMsg = document.getElementById('endgameMsg')
+const overlay = document.getElementById('overlay')
+const restartButton = document.getElementById('restartButton')
+
+rockButton.addEventListener('click', () => handleClick('ROCK'))
+paperButton.addEventListener('click', () => handleClick('PAPER'))
+scissorsButton.addEventListener('click', () => handleClick('SCISSORS'))
+restartButton.addEventListener('click', restartGame)
+overlay.addEventListener('click', closeEndgameModal)
+
+function handleClick(playerPlay) {
+  if (gameOver()) {
+    openEndgameModal()
+    return
   }
-  
-  let playerScore = 0;
-  let computerScore = 0;
-  
-  function gameWinner() {
-    while (playerScore < 5 && computerScore < 5) {
-      let result = playRound(playerPlay(), computerPlay());
-  
-      if (result == "player") {
-        playerScore++;
-        alert(`Player won the round, ${playerScore} points`);
-      } else if (result == "computer") {
-        computerScore++;
-        alert(`Computer won the round, ${computerScore} points`);
-      } else if (result == "tie") {
-        alert("It's a tie, play again!");
-      }
-    }
-  
-    if (playerScore == 5) {
-      alert("Player won the game!");
-    } else if (computerScore == 5) {
-      alert("Computer won the game!");
-    }
+
+  const cpuPlay = computerPlay()
+  playRound(playerPlay, cpuPlay)
+  updateChoices(playerPlay, cpuPlay)
+  updateScore()
+
+  if (gameOver()) {
+    openEndgameModal()
+    setFinalMessage()
   }
-  
-  gameWinner();
+}
+
+function updateChoices(playerPlay, computerPlay) {
+  switch (playerPlay) {
+    case 'ROCK':
+      playerChose.innerHTML = '<div class="hand"><img src="./img/rock.png" alt="rock"></div>';
+      break;
+    case 'PAPER':
+      playerChose.innerHTML = '<div class="hand"><img src="./img/paper.png" alt="paper"></div>';
+      break;
+    case 'SCISSORS':
+      playerChose.innerHTML = '<div class="hand"><img src="./img/scissors.png" alt="scissors"></div>';
+      break;
+  }
+
+  switch (computerPlay) {
+    case 'ROCK':
+      cpuChose.innerHTML = '<div class="hand"><img src="./img/rock.png" alt="rock"></div>';
+      break;
+    case 'PAPER':
+      cpuChose.innerHTML = '<div class="hand"><img src="./img/paper.png" alt="paper"></div>';
+      break;
+    case 'SCISSORS':
+      cpuChose.innerHTML = '<div class="hand"><img src="./img/scissors.png" alt="scissors"></div>';
+      break;
+  }
+}
+
+function updateScore() {
+  if  (roundWinner === 'tie') {
+    scoreInfo.textContent = "It's a tie!"
+  } else if (roundWinner === 'player') {
+    scoreInfo.textContent = 'Player win!'
+  } else if (roundWinner === 'computer') {
+    scoreInfo.textContent = 'Computer win!'
+  }
+
+  playerScorePara.textContent = `Player: ${playerScore}`
+  computerScorePara.textContent = `Computer: ${computerScore}`
+}
+
+function updateScoreMessage (winner, playerPlay, computerPlay) {
+  if (winner === 'player') {
+    scoreMessage.textContent = `${capitalizeFirstLetter(playerPlay)}
+    beats ${computerPlay.toLowerCase()}`
+    return
+  }
+  if (winner === 'computer') {
+    scoreMessage.textContent = `${capitalizeFirstLetter(computerPlay)}
+    beats ${playerPlay.toLowerCase()}`
+    return
+  }
+  scoreMessage.textContent = `${capitalizeFirstLetter(
+    playerPlay
+  )} ties with ${computerPlay.toLowerCase()}`
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function openEndgameModal() {
+  endgameModal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeEndgameModal() {
+  endgameModal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+function setFinalMessage() {
+  return playerScore > computerScore
+  ? (endgameMsg.textContent = "Congrats, you won!")
+  : (endgameMsg.textContent = "You lost, try again!")
+}
+
+function restartGame() {
+  playerScore = 0
+  computerScore = 0
+  scoreInfo.textContent = 'Choose your weapon'
+  scoreMessage.textContent = 'First to score 5 points wins the game'
+  playerScorePara.textContent = 'Player: 0'
+  computerScorePara.textContent = 'Computer: 0'
+  endgameModal.classList.remove('active')
+  overlay.classList.remove('active')
+}
